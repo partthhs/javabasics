5cb219629e90f1b2f0519a4a2ddba424e60785b8
partthhs <parthh989@gmail.com>
2026-02-08 22:24:33 +0530

    Relational operator code added

---
+++ Relational.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Relational
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the first number=");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number=");
+        int b=sc.nextInt();
+        System.out.println("a>b="+(a>b));
+        System.out.println("a<b="+(a<b));
+        System.out.println("a>=b="+(a>=b));
+        System.out.println("a<=b="+(a<=b));
+        System.out.println("a==b="+(a==b));
+        System.out.println("a!=b="+(a!=b));
+    }
+}
+    
